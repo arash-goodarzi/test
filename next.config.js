2ba939a5cfbb645ebbacd3cc45da4a635f6ec81f
Arash Goodarzi <aras.goodarzi@gmail.com>
2024-01-16 10:00:22 +0330

    modify nextCongif

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   skipTrailingSlashRedirect: true,
  
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  // distDir: 'dist',
 }
  
 module.exports = nextConfig
